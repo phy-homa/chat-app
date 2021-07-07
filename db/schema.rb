f50f83e2701b0a16e3c752a045beeaa4dc8d21ac
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-07 22:29:53 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -22,15 +22,15 @@ ActiveRecord::Schema.define(version: 2021_07_07_122450) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "user", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
