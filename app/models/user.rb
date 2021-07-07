803a6e9cb6a61778b8b53e6d58866ef40b92878f
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-07 21:37:22 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  
+  has_many room_user
+  has_many :rooms, throught: :room_user
 end
