4394d69bb21bcaaf7a78c1e59930b35921a4854d
phy-homa <86774819+phy-homa@users.noreply.github.com>
2021-07-08 22:23:58 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "rooms#index"  #基本画面設定
   resources :users, only: [:edit, :update, :destroy]
-  resources :rooms, only: [:new,:create,:index]
+  resources :rooms, only: [:new,:create] do
+    resources :messages, only: [:create,:index]
+  end
 end
